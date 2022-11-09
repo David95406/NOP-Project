8ad2f1f2d23f4583ca48db8d6042bfbb1e47816c
DetariLeon <detari.leon.2020i@bankitatabanya.hu>
2022-11-09 12:24:28 +0100

    3,4 feladat
    O

--- NOP-Project/src/nop/project/NOPProject.java
+++ NOP-Project/src/nop/project/NOPProject.java
@@ -2,8 +2,17 @@ package nop.project;
 
 public class NOPProject {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void elvalszt() {
+        System.out.println("*****************");
+
     }
-    
+
+    public static boolean szokoev(int ev) {
+        return ((ev > 1582) && (ev % 4 == 0) && (ev % 100 != 0) || (ev % 400 == 0));
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
